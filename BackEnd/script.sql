f245e120e3947be61f9717d8ad4f1646633a3a6a
Ariaz13 <zaira_c.j@hotmail.com>
2020-12-02 23:57:17 -0600

    Corrección de tabla Cuentas, Instituciones y Usuarios

--- BackEnd/script.sql
+++ BackEnd/script.sql
@@ -12,6 +12,13 @@ CREATE TABLE `perritos`.`callejeros` (
   `condicion` VARCHAR(120) NOT NULL,
   `estatus` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idcallejeros`));
+  
+CREATE TABLE `perritos`.`cuentas` (
+  `idcuentas` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(45) NOT NULL,
+  `contrasena` VARCHAR(8) NOT NULL,
+  `tipo_usuario` INT NOT NULL,
+  PRIMARY KEY (`idcuentas`));
 
 CREATE TABLE `perritos`.`instituciones` (
   `idinstituciones` INT NOT NULL AUTO_INCREMENT,
@@ -23,20 +30,26 @@ CREATE TABLE `perritos`.`instituciones` (
   `cargo_representante` VARCHAR(45) NOT NULL,
   `tipo_institucion` VARCHAR(45) NOT NULL,
   `identificacion_tributaria` VARCHAR(45) NOT NULL,
-  `tipo_usuario` INT NOT NULL,
-  PRIMARY KEY (`idinstituciones`));
+  `idcuentas` INT NOT NULL,
+  PRIMARY KEY (`idinstituciones`),
+  CONSTRAINT `idcuentai`
+    FOREIGN KEY (`idcuentas`)
+    REFERENCES `perritos`.`cuentas` (`idcuentas`));
 
 CREATE TABLE `perritos`.`usuarios` (
   `idusuarios` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
   `apellidos` VARCHAR(45) NOT NULL,
   `genero` VARCHAR(45) NOT NULL,
-  `telefono` VARCHAR(10) NOT NULL,
+  `telefono_usuario` VARCHAR(10) NOT NULL,
   `direccion` VARCHAR(100) NOT NULL,
   `codigo_postal` VARCHAR(5) NOT NULL,
   `email` VARCHAR(45) NOT NULL,
-  `tipo_usuario` INT NOT NULL,
-  PRIMARY KEY (`idusuarios`));
+  `idcuentas` INT NOT NULL,
+  PRIMARY KEY (`idusuarios`),
+  CONSTRAINT `idcuentau`
+    FOREIGN KEY (`idcuentas`)
+    REFERENCES `perritos`.`cuentas` (`idcuentas`));
 
 CREATE TABLE `perritos`.`adopciones` (
   `idadopciones` INT NOT NULL AUTO_INCREMENT,
@@ -47,9 +60,6 @@ CREATE TABLE `perritos`.`adopciones` (
   `ocupacion_adoptante` VARCHAR(45) NOT NULL,
   `ingresos_adoptante` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idadopciones`),
-  INDEX `idusuarios_idx` (`idusuarios` ASC),
-  INDEX `idcallejeros_idx` (`idcallejeros` ASC),
-  INDEX `idinstituciones_idx` (`idinstituciones` ASC),
   CONSTRAINT `idusuarios`
     FOREIGN KEY (`idusuarios`)
     REFERENCES `perritos`.`usuarios` (`idusuarios`),
@@ -60,26 +70,16 @@ CREATE TABLE `perritos`.`adopciones` (
     FOREIGN KEY (`idinstituciones`)
     REFERENCES `perritos`.`instituciones` (`idinstituciones`));
 
-CREATE TABLE `perritos`.`cuentas` (
-  `idcuentas` INT NOT NULL AUTO_INCREMENT,
-  `id` INT NOT NULL,
-  `usuario` VARCHAR(45) NOT NULL,
-  `contrasena` VARCHAR(8) NOT NULL,
-  PRIMARY KEY (`idcuentas`),
-  INDEX `id_idx` (`id` ASC),
-  CONSTRAINT `id_i`
-    FOREIGN KEY (`id`)
-    REFERENCES `perritos`.`instituciones` (`idinstituciones`),
-  CONSTRAINT `id_u`
-    FOREIGN KEY (`id`)
-    REFERENCES `perritos`.`usuarios` (`idusuarios`));
 
---Join tabla adoptados, usuarios e instituciones
-SELECT
+ 
+USE perritos 
+
+-- Join para consulta adoptados
+SELECT 
 adopciones.idadopciones,
 usuarios.nombre,
 usuarios.apellidos,
-usuarios.direccion,
+usuarios.direccion, 
 usuarios.codigo_postal,
 usuarios.telefono,
 usuarios.genero,
@@ -95,4 +95,3 @@ INNER JOIN instituciones
 ON adopciones.idinstituciones = instituciones.idinstituciones
 INNER JOIN callejeros
 ON adopciones.idcallejeros = callejeros.idcallejeros
-
